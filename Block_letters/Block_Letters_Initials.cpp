22bd586358063416e1472b34c3e345b0af605e20
Zach <Zabloom01@gmail.com>
2024-11-17 11:17:56 -0500

    Create Block_Letters_Initials.cpp
    ASCII Art for initials.

---
+++ Block_letters/Block_Letters_Initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  std::cout << " ZZZZZZZZZZZZZ  BBBBBBBBBBBBB   \n";
+  std::cout << " ZZZZZZZZZZZZZ  B           BB  \n";
+  std::cout << "         ZZ     B            BB \n";
+  std::cout << "       ZZ       BBBBBBBBBBBBB   \n";
+  std::cout << "     ZZ         BBBBBBBBBBBBB   \n";
+  std::cout << "   ZZ           B            BB \n";
+  std::cout << " ZZZZZZZZZZZZZ  B           BB  \n";
+  std::cout << " ZZZZZZZZZZZZZ  BBBBBBBBBBBBB   \n";
+
+  return 0;
+}
