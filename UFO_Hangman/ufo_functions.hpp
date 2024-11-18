54bbc641656fca126797adc978018120bc2c041e
Zach <Zabloom01@gmail.com>
2024-11-18 00:28:13 -0500

    Create ufo_functions.hpp

---
+++ UFO_Hangman/ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+// Declare all function here
+void display_misses(int misses);
+void greet(); // declare greet function, no parameters
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword); 
+ // delare the end game function
